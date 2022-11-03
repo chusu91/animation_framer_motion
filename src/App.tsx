7b13bfb91b65f9a622a098d6b85b0ee88507a7d3
Lucy <chusu91@gmail.com>
2022-11-04 00:10:06 +0200

    Slider animation

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
@@ -17,43 +18,74 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   position: absolute;
-  top: 100px;
+  top: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial: {
+// to use custom, variants must be the function
+// the fuction has one argument and it comes from custom prop
+// to return the value of fuction, const ()=> ({}) or const ()=>{return{}}
+const box = {
+  entry: (isBack: boolean) => ({
+    x: isBack ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+    transition: {
+      duration: 0.3,
+    },
+  }),
+  center: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 0.3,
+    },
   },
-  leaving: {
-    opacity: 0,
-    scale: 0,
-    y: 100,
+  exit: (isBack: boolean) => {
+    return {
+      x: isBack ? 500 : -500,
+      opacity: 0,
+      scale: 0,
+      transition: {
+        duration: 0.3,
+      },
+    };
   },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
-      <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+      <AnimatePresence mode="wait" custom={back}>
+        <Box
+          custom={back} // state value send as prop
+          variants={box}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible} // 키가 변하면 리렌더링. 새로운 컴포넌트가 생겼다고 생각하고 이전꺼 없앰.
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
+      <button onClick={prevPlease}>prev</button>
     </Wrapper>
   );
 }
